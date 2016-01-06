089e3f4d9a7b8b5b07aff54b0fb67f34cb57cc2e
ant-kord <ant-kord@yandex.ru>
2016-01-06 19:57:16 +0600

    Rejected commit: Default

---
+++ src/main/java/ru/antkord/webshop/model/BaseEntity.java
@@ -0,0 +1,25 @@
+package ru.antkord.webshop.model;
+
+
+import javax.persistence.*;
+
+/**
+ * Created by ant-kord on 06.01.2016.
+ */
+@MappedSuperclass
+public class BaseEntity {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Integer id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+}
