771f9faf48f96a2dd880c2ad1f86ae2f5b5e7d6c
ant-kord <ant-kord@yandex.ru>
2016-01-06 19:57:16 +0600

    Rejected commit: Default

---
+++ src/main/java/ru/antkord/webshop/repository/ProductRepository.java
@@ -0,0 +1,21 @@
+package ru.antkord.webshop.repository;
+
+import org.springframework.dao.DataAccessException;
+import ru.antkord.webshop.model.Product;
+
+import java.util.Collection;
+
+/**
+ * Created by ant-kord on 06.01.2016.
+ */
+public interface ProductRepository {
+
+    Collection<Product> getProductByCatalogType(int pageNumber, int pageLimit) throws DataAccessException;
+
+    Collection<Product> getProductNew(boolean isNew) throws DataAccessException;
+
+    Product findProductById(int id) throws DataAccessException;
+
+    Product findProductName(String name) throws DataAccessException;
+
+}
