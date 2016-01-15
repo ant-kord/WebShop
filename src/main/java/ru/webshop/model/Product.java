b03385c7430fd3489e061d9f33988a3df9e5936e
ant-kord <ant-kord@yandex.ru>
2016-01-15 21:48:33 +0600

    Rejected commit: Default

--- src/main/java/ru/webshop/model/Product.java
+++ src/main/java/ru/webshop/model/Product.java
@@ -121,4 +121,26 @@ public class Product extends NamedEntity{
     public void setIsNew(boolean isNew) {
         this.isNew = isNew;
     }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 59 * hash + (this.id != null ? this.id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Product other = (Product) obj;
+        if ((this.id == null) ? (other.id != null) : !this.id.equals(other.id)) {
+            return false;
+        }
+        return true;
+    }
 }
